b5c92d2684d039793b12a6c877e463d509e7ae04
NicoleBorbe <nicole.borbe@gmail.com>
2022-11-01 21:52:06 +0100

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let array = [
+    3.14,
+    193.4464,
+    0.8596433607,
+    -2.940629089,
+  ];
+
+function runden() {
+    for (x = 0; x < array.length; x++) {
+        console.log(Math.round(array[x]));
+        // console.log("Test");
+    }
+};
+runden();
+
